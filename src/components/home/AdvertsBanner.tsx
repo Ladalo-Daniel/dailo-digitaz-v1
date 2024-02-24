6caedbe254070c33053e13655e69457d3f4f8c1e
Ladalo-Daniel <balaladalo@gmail.com>
2024-02-24 23:43:52 +0100

    work hard

--- src/components/home/AdvertsBanner.tsx
+++ src/components/home/AdvertsBanner.tsx
@@ -6,8 +6,8 @@ import Image from 'next/image'
 
 function AdvertsBanner() {
   return (
-    <MaxWrapper  className='py-5 bg-slate-100 max-w-7xl md:py-8 px-0 md:px-14'>
-        <Card className=' p-2 md:p-4'>
+    <MaxWrapper  className='py-8 bg-red-100 max-w-7xl md:py-8 px-0 md:px-14'>
+        <Card className=' p-2 md:p-4 bg-red-400'>
             <AspectRatio ratio={16/9}>
                 <Image src="/slides/6.gif" alt='advert banner' fill quality={100} className=' rounded-md' />
             </AspectRatio>
