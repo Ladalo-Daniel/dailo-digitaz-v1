55c3f6dd55601b022faec03c10ff25cddb18a522
Ladalo-Daniel <balaladalo@gmail.com>
2024-02-26 17:57:57 +0100

    we are making progress

---
+++ src/app/dashboard/add-product/page.tsx
@@ -0,0 +1,14 @@
+import MaxWrapper from '@/components/MaxWrapper'
+import React from 'react'
+
+const AddProductPage = () => {
+  return (
+    <MaxWrapper className="bg-gray-100 md:mt-[5px] mt-12 px-0 md:px-2  flex-1 max-w-7xl">
+        <div className="md:py-5 py-2">
+          AddProductPage
+        </div>
+    </MaxWrapper>
+  )
+}
+
+export default AddProductPage
--- src/components/home/AdvertsBanner.tsx
+++ src/components/home/AdvertsBanner.tsx
@@ -6,10 +6,15 @@ import Image from 'next/image'
 
 function AdvertsBanner() {
   return (
-    <MaxWrapper  className='py-8 bg-red-100 max-w-7xl md:py-8 px-0 md:px-14'>
-        <Card className=' p-2 md:p-4 bg-red-400'>
+    <MaxWrapper  className='py-8 bg-red-100 max-w-7xl md:py-8 px-0 md:px-14 flex flex-col gap-5'>
+        <Card className=' p-2 md:p-4 bg-gray-100'>
             <AspectRatio ratio={16/9}>
                 <Image src="/slides/6.gif" alt='advert banner' fill quality={100} className=' rounded-md' />
+            </AspectRatio>
+        </Card>
+        <Card className=' p-2 md:p-4 bg-gray-100'>
+            <AspectRatio ratio={16/6}>
+                <Image src="/slides/8.gif" alt='advert banner' fill quality={100} className=' rounded-md' />
             </AspectRatio>
         </Card>
     </MaxWrapper>
